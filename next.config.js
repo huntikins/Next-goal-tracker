94e9feb380e35dd363282ef957914f3ce5f8743c
Hunter Trammell <hunterttrammell@gmail.com>
2021-08-25 18:38:12 -0500

    Update: Generate Next.js ffiles for starting template

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	env: {
+		mongo_user: 'username',
+		mongo_pass: 'password',
+		mongo_cluster: 'cluster',
+		mongo_database: 'database',
+		baseurl: 'http://localhost:3000',
+	},
+}
